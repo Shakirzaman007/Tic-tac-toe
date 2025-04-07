0fe0098b1f7f00c6d38c43e1d2fa0189ea5870e2
Shakirzaman007 <shakir071096@gmail.com>
2025-04-07 15:26:15 +0600

    win

--- tic.js
+++ tic.js
@@ -1,5 +1,8 @@
 let boxes = document.querySelectorAll(".box");
 let resetBtn = document.querySelector("#reset-btn");
+let newGameBtn = document.querySelector("#new-btn");
+let msgContainer = document.querySelector(".msg-container");
+let msg = document.querySelector("#msg");
 
 let turnO = true;
 
@@ -30,6 +33,11 @@ boxes.forEach((box) => {
   });
 });
 
+const showWinner = (winner) => {
+    msg.innerText = `Congratulations, Winner is ${winner}`;
+    msgContainer.classList.remove("hide");
+};
+
 const checkWinner = () => {
   for (let pattern of winPatterns) {
     let pos1Val = boxes[pattern[0]].innerText;
@@ -39,6 +47,7 @@ const checkWinner = () => {
     if (pos1Val != "" && pos2Val != "" && pos3Val != "") {
       if (pos1Val === pos2Val && pos2Val === pos3Val) {
         console.log("winner", pos1Val);
+        showWinner(pos1Val);
       }
     }
   }
